69c0cd7a61ae9288de951f9ecc65979adc18201c
Davide Cavalca <davide@cavalca.name>
2023-11-09 21:45:26 -0500

    Abstract system-specific logic and add mock
    Define StartupDiskTrait to abstract system-specific logic; define two
    implementations for it, AsahiBlessLibrary (leveraging asahi_bless for
    Apple Silicon) and MockLibrary (just a stub returning mock data).
    
    Define StartupDiskLibrary which implements the trait and dispatches
    between the available implementations. By default MockLibrary will be
    used if USE_MOCK_LIBRARY is defined in the environment, otherwise
    AsahiBlessLibrary will be used.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use asahi_bless::{get_boot_candidates, set_boot_volume};
+use asahi_bless::BootCandidate;
+use asahi_bless::Error;
 use gtk::prelude::*;
 use gtk::{glib, Application, ApplicationWindow, Box, Orientation, ToggleButton};
+use rand::Rng;
+use std::env;
+use uuid::Uuid;
 
 const APP_ID: &str = "org.gnome.StartupDisk";
 
@@ -15,13 +19,106 @@ fn main() -> glib::ExitCode {
     app.run()
 }
 
-fn build_ui(app: &Application) {
-    // We need root for this to do anything useful
-    sudo::escalate_if_needed().unwrap();
+type Result<T> = std::result::Result<T, Error>;
 
+fn generate_random_strings(num_strings: usize, max_length: usize) -> Vec<String> {
+    let mut rng = rand::thread_rng();
+
+    (0..num_strings)
+        .map(|_| {
+            let string_length = rng.gen_range(1..=max_length);
+            (0..string_length)
+                .map(|_| rng.gen_range(b'a'..=b'z') as char)
+                .collect()
+        })
+        .collect()
+}
+
+trait StartupDiskTrait {
+    fn get_boot_candidates(&self) -> Result<Vec<BootCandidate>>;
+    fn set_boot_volume(&self, cand: &BootCandidate, next: bool) -> Result<()>;
+}
+
+struct AsahiBlessLibrary;
+impl StartupDiskTrait for AsahiBlessLibrary {
+    fn get_boot_candidates(&self) -> Result<Vec<BootCandidate>> {
+        // We need root for this to do anything useful
+        sudo::escalate_if_needed().unwrap();
+        return asahi_bless::get_boot_candidates();
+    }
+
+    fn set_boot_volume(&self, cand: &BootCandidate, next: bool) -> Result<()> {
+        // We need root for this to do anything useful
+        sudo::escalate_if_needed().unwrap();
+        return asahi_bless::set_boot_volume(cand, next);
+    }
+}
+
+struct MockLibrary;
+impl StartupDiskTrait for MockLibrary {
+    fn get_boot_candidates(&self) -> Result<Vec<BootCandidate>> {
+        let mut cands: Vec<BootCandidate> = Vec::new();
+        cands.push(BootCandidate {
+            vg_uuid: Uuid::new_v4(),
+            vol_names: generate_random_strings(2, 10),
+            part_uuid: Uuid::new_v4(),
+        });
+        cands.push(BootCandidate {
+            vg_uuid: Uuid::new_v4(),
+            vol_names: generate_random_strings(2, 10),
+            part_uuid: Uuid::new_v4(),
+        });
+        cands.push(BootCandidate {
+            vg_uuid: Uuid::new_v4(),
+            vol_names: generate_random_strings(2, 10),
+            part_uuid: Uuid::new_v4(),
+        });
+        return Ok(cands);
+    }
+
+    fn set_boot_volume(&self, cand: &BootCandidate, next: bool) -> Result<()> {
+        println!(
+            "Setting boot volume: {} {}",
+            cand.vol_names.join(", "),
+            next
+        );
+        return Ok(());
+    }
+}
+
+enum StartupDiskLibrary {
+    AsahiBless(AsahiBlessLibrary),
+    Mock(MockLibrary),
+}
+
+impl StartupDiskTrait for StartupDiskLibrary {
+    fn get_boot_candidates(&self) -> Result<Vec<BootCandidate>> {
+        match self {
+            StartupDiskLibrary::AsahiBless(lib) => lib.get_boot_candidates(),
+            StartupDiskLibrary::Mock(lib) => lib.get_boot_candidates(),
+        }
+    }
+    fn set_boot_volume(&self, cand: &BootCandidate, next: bool) -> Result<()> {
+        match self {
+            StartupDiskLibrary::AsahiBless(lib) => lib.set_boot_volume(cand, next),
+            StartupDiskLibrary::Mock(lib) => lib.set_boot_volume(cand, next),
+        }
+    }
+}
+
+fn build_ui(app: &Application) {
     let container = Box::new(Orientation::Horizontal, 10);
-    let cands = get_boot_candidates().unwrap();
     let mut last_button: Option<ToggleButton> = None;
+
+    let use_mock_library = env::var("USE_MOCK_LIBRARY").is_ok();
+    // Create an instance of the chosen implementation
+    let startup_disk_library: &dyn StartupDiskTrait = if use_mock_library {
+        &StartupDiskLibrary::Mock(MockLibrary)
+    } else {
+        &StartupDiskLibrary::AsahiBless(AsahiBlessLibrary)
+    };
+
+    let cands = startup_disk_library.get_boot_candidates().unwrap();
 
     for cand in cands {
         let button = ToggleButton::builder()
@@ -34,7 +131,7 @@ fn build_ui(app: &Application) {
 
         // Connect to "clicked" signal of `button`
         button.connect_clicked(move |_button| {
-            set_boot_volume(&cand, false).unwrap();
+            startup_disk_library.set_boot_volume(&cand, false).unwrap();
         });
 
         if let Some(ref last) = last_button {
