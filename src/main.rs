c137829192a1049ed62d2a70072a03dfa85d8b67
Davide Cavalca <davide@cavalca.name>
2023-11-10 22:27:26 -0500

    Use Libadwaita more and refactor

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,30 @@
 use adw::prelude::*;
-use adw::Application;
-use gtk::{gio, glib, ApplicationWindow, Box, Label, Orientation, ToggleButton};
+use adw::{Application, ApplicationWindow, HeaderBar};
+use gtk::{gio, glib, Box, Label, Orientation, ToggleButton};
 use startup_disk::startup_disk_library;
 
+const APP_NAME: &str = "Startup Disk";
 const APP_ID: &str = "org.gnome.StartupDisk";
 
 fn main() -> glib::ExitCode {
     // Create a new application
-    let app = adw::Application::builder().application_id(APP_ID).build();
+    let app = Application::builder().application_id(APP_ID).build();
 
     // Connect to "activate" signal of `app`
     app.connect_activate(build_ui);
 
     // Hook up actions
-    app.set_accels_for_action("app.quit", &["<primary>q"]);
     let quit_action = gio::ActionEntry::builder("quit")
         .activate(|app: &Application, _, _| app.quit())
         .build();
     app.add_action_entries([quit_action]);
+    app.set_accels_for_action("app.quit", &["<primary>q"]);
 
     // Run the application
     app.run()
 }
 
-fn build_window(app: &Application) -> ApplicationWindow {
-    let container = Box::new(Orientation::Vertical, 2);
+fn build_boot_candidates_box() -> Box {
     let buttons_container = Box::new(Orientation::Horizontal, 10);
     let mut last_button: Option<ToggleButton> = None;
 
@@ -53,6 +53,12 @@ fn build_window(app: &Application) -> ApplicationWindow {
         last_button = Some(button);
     }
 
+    buttons_container
+}
+
+fn build_app_window(app: &Application) -> ApplicationWindow {
+    let content = Box::new(Orientation::Vertical, 0);
+
     let label = Label::builder()
         .label("Select the disk you want to use to start up from")
         .margin_top(12)
@@ -61,14 +67,15 @@ fn build_window(app: &Application) -> ApplicationWindow {
         .margin_end(12)
         .build();
 
-    container.append(&label);
-    container.append(&buttons_container);
+    content.append(&HeaderBar::new());
+    content.append(&label);
+    content.append(&build_boot_candidates_box());
 
     // Create a window
     ApplicationWindow::builder()
         .application(app)
-        .title("Startup Disk")
-        .child(&container)
+        .title(APP_NAME)
+        .content(&content)
         .build()
 }
 
@@ -77,7 +84,7 @@ fn build_ui(app: &Application) {
     let window = if let Some(window) = app.active_window() {
         window
     } else {
-        let window = build_window(app);
+        let window = build_app_window(app);
         window.upcast()
     };
 
