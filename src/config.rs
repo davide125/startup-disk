eebcbfcaebc3ade1f8ec19fd33b7de64122e8d23
Davide Cavalca <davide@cavalca.name>
2023-12-15 10:51:27 -0800

    Use crate version instead of hardcoding

--- src/config.rs
+++ src/config.rs
@@ -2,5 +2,5 @@
 
 pub static APP_NAME: &str = "Startup Disk";
 pub static APP_ID: &str = "org.gnome.StartupDisk";
-pub static APP_VERSION: &str = "0.1.0";
+pub static APP_VERSION: &str = env!("CARGO_PKG_VERSION");
 pub static RESOURCE_BASE: &str = "/org/gnome/startup-disk";
