2980c479ec9f7e5f4e4fb6b5b750470511280020
Davide Cavalca <davide@cavalca.name>
2026-02-28 08:24:38 -0800

    Use nix instead of libc to detect root

--- src/startup_disk/asahi.rs
+++ src/startup_disk/asahi.rs
@@ -6,7 +6,7 @@ use crate::startup_disk::is_asahi;
 use crate::startup_disk::Result;
 use crate::startup_disk::StartupDiskTrait;
 use crate::{privileged, startup_disk::StartupDiskError};
-use libc;
+use nix::unistd::geteuid;
 
 pub struct AsahiBlessLibrary;
 impl StartupDiskTrait for AsahiBlessLibrary {
@@ -15,7 +15,7 @@ impl StartupDiskTrait for AsahiBlessLibrary {
     }
 
     fn get_boot_candidates(&self) -> Result<Vec<BootCandidate>> {
-        if is_root() {
+        if geteuid().is_root() {
             asahi_bless::get_boot_candidates().map_err(StartupDiskError::from)
         } else {
             privileged::get_boot_candidates()
@@ -23,7 +23,7 @@ impl StartupDiskTrait for AsahiBlessLibrary {
     }
 
     fn get_boot_volume(&self, device: &str, next: bool) -> Result<BootCandidate> {
-        if is_root() {
+        if geteuid().is_root() {
             asahi_bless::get_boot_volume(device, next).map_err(StartupDiskError::from)
         } else {
             privileged::get_boot_volume(device, next)
@@ -31,15 +31,10 @@ impl StartupDiskTrait for AsahiBlessLibrary {
     }
 
     fn set_boot_volume(&self, device: &str, cand: &BootCandidate, next: bool) -> Result<()> {
-        if is_root() {
+        if geteuid().is_root() {
             asahi_bless::set_boot_volume(device, cand, next).map_err(StartupDiskError::from)
         } else {
             privileged::set_boot_volume(device, cand, next)
         }
     }
-}
-
-fn is_root() -> bool {
-    // Should be fairly safe as the libc call has no side effects beyond querying effective UID
-    unsafe { libc::geteuid() == 0 }
 }
